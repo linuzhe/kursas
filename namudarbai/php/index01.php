b516caf8f10ad7800d602e9a382ac7101d07c2fd
linuzhe <linuzhe@gmail.com>
2019-05-06 22:42:13 +0300

    php index01

---
+++ namudarbai/php/index01.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 1 paskaita: užduotis Index01 | Linos darbai  </title>  
+</head> 
+    
+    
+<body>
+    
+<?php    
+    $country = 'Lietuva'; 
+    $city = 'Vilnius'; 
+    $population = 574221; 
+?>
+    <div>    
+        
+        <ul>
+            <li><?php echo $country; ?></li>
+            <li><?php echo $city ?></li>
+            <li><?php echo $population ?></li>
+        </ul> 
+        
+    </div>
+    
+</body>
+    
+</html>
