d8fbe395aa556a5f71bc7675b17cb2a7ca44e2df
linuzhe <linuzhe@gmail.com>
2019-05-06 23:35:41 +0300

    1 php paskaita 3 uzduotis

---
+++ namudarbai/php/index03.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 1 paskaita: užduotis Index03 | Linos darbai  </title>  
+</head>     
+    
+<body>
+    
+    <?php    
+         $x = 10; 
+         $y = 7;   
+    ?>
+    
+    <div>          
+        <p><?php $z = $x + $y; echo "$x + $y = $z"; ?></p>     
+        <p><?php $z = $x - $y; echo "$x - $y = $z"; ?></p>   
+        <p><?php $z = $x / $y; echo "$x / $y = $z"; ?></p>  
+        <p><?php $z = $x % $y; echo "$x % $y = $z"; ?></p>  
+    </div>  
+    
+</body>
+    
+</html>
+
