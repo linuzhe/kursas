60529948ae84868c450a24d3c28de581626776e9
linuzhe <linuzhe@gmail.com>
2019-05-06 23:08:45 +0300

    php index02

---
+++ namudarbai/php/index02.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 1 paskaita: užduotis Index02 | Linos darbai  </title>  
+</head>     
+    
+<body>
+    
+    <?php    
+        $country = 'Lietuva'; 
+        $city = 'Vilnius'; 
+        $population = 574221; 
+    ?>
+    
+    <div>  
+        <p><?php echo $city . ' – Lietuvos sostinė.'; ?></p>    
+        <p><?php echo "$city ir $country sparčiai auga."; ?></p>     
+    </div>
+    
+</body>
+    
+</html>
+
