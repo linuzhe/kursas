9712ca055aa1f60aa11f3f881f6a20881a628b72
Lina Stankienė <lina@infolex.lt>
2019-05-13 17:21:37 +0300

    projekto pirmas juodrastis

---
+++ namudarbai/projektas/index.php
@@ -0,0 +1,75 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Renginio pavadinimas | Linos projektas</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet"> 
+</head>
+
+<body>
+    
+    <header class="header" id="header">   
+        <div class= "top">
+            
+            <div class="logo">Logo</div>
+
+         <div class="menu">
+                 <div class="menuItem"><a href="index.php">Apie renginį</a></div>
+                 <div class="menuItem"><a href="programa.php">Programa</a></div>
+                 <div class="menuItem"><a href="komanda.php">Komanda</a></div>
+                 <div class="menuItem"><a href="registracija.php">Registracija</a></div>  
+                 <div class="menuItem"><a href="kaina.php">Kaina</a></div>
+                 <div class="menuItem"><a href="kontaktai.php">Kontaktai</a></div>    
+            </div>
+            
+        </div>
+    </header>
+    <p>test</p>
+    <section id="about">
+        <div>        
+            <h1>Apie renginį</h1>
+        </div>  
+    </section>
+    
+    <section id="team">
+        <div>        
+            <h1>Komanda</h1>
+        </div>    
+    </section>
+    
+    <section id="programme">
+        <div>        
+            <h1>Programa</h1>
+        </div>    
+    </section>
+    
+    <section id="price">
+        <div>        
+            <h1>Kaina</h1>
+        </div>    
+    </section>
+    
+     <section id="registration">
+        <div>        
+            <h1>Registracija</h1>
+        </div>    
+    </section>
+    
+    <footer>
+        <div>
+            <p>2019 metai</p>
+        </div>
+    </footer>  
+    
+    
+</body>
+    
+</html>
+
+
