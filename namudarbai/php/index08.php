babdede2b37050ff157a6fa75b04d9be0ae57adc
linuzhe <linuzhe@gmail.com>
2019-05-19 23:28:10 +0300

    namu darbas php8

--- namudarbai/php/index08.php
+++ namudarbai/php/index08.php
@@ -11,29 +11,30 @@
     <div> 
         
         <h1>Temperatūros Vilniuje balandžio mėnesį:</h1>
-    
-        <?php    
-
-        $temp = array(4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21);
-        rsort($temp);      
-
-        foreach($temp as $x => $temperatura)  {
-            echo  'Balandžio ' . ($x+1) . ' Vilniuje buvo ' . $temperatura . ' laipsniai(-ių).';
-            echo "<br>";
-        }
-        ?>    
- 
-<!--        UZDUOTIS NEBAIGTA    -->
-    
-        <!--
-        - Sukurk masyvą temp ir patalpink jame Vilniaus balandžio mėnesio oro temperatūras:
-        - 4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21.
-        - Rask ir išvesk vidutinę mėnesio temperatūrą, penkias šilčiausias ir penkias šalčiausias temperatūras.
-        - Patarimai:
-        1. Naudok foreach ciklą.
-        2. Naudok round( ), kad suapvalintum vidutinę temperatūrą.
-        3. Naudok rsort( ), kad surūšiuoti ciklą žemėjančia tvarka.
-        4. Naudok array_slice( ), kad surastum penkias šilčiausias ir penkias šalčiausias temperatūras.
+        
+        <?php 
+            $temp = array(4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21);
+            $temp2 = array(4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21);
+            $suma = array_sum($temp); 
+            $average = $suma / count($temp);                  
+            rsort($temp2);
+            echo '<h2>Didžiausia temperatūra buvo: ' . implode(', ', array_slice($temp2,0,5)) . ' laipsniai.<br> Mažiausia temperatūra: ' . implode(', ', array_slice($temp2,-5,5)) . ' laipsniai. </h2>';
+        
+            echo '<h2>Vidutinė balandžio temperatūra: ' . round($average) . ' laipsnių, nes:</h2>';
+            foreach($temp as $x => $temperatura)  {
+                echo  'balandžio ' . ($x+1) . ' Vilniuje buvo ' . $temperatura . ' laipsniai(-ių). <br>';
+                }            
+        ?> 
+       
+        <!--     
+            - Sukurk masyvą temp ir patalpink jame Vilniaus balandžio mėnesio oro temperatūras:
+            - 4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15, 19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21.
+            - Rask ir išvesk vidutinę mėnesio temperatūrą, penkias šilčiausias ir penkias šalčiausias temperatūras.
+            - Patarimai:
+            1. Naudok foreach ciklą.
+            2. Naudok round( ), kad suapvalintum vidutinę temperatūrą.
+            3. Naudok rsort( ), kad surūšiuoti ciklą žemėjančia tvarka.
+            4. Naudok array_slice( ), kad surastum penkias šilčiausias ir penkias šalčiausias temperatūras.
         -->
         
     </div>
